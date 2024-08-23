c60a79010390c6ce376979261b6f9536aa0b1baf
jjungcu519 <jjungcu0519@gmail.com>
2024-08-23 10:54:05 +0900

    update

---
+++ 0.wordcount/reducer.py
@@ -0,0 +1,24 @@
+import sys
+
+last_word = None
+total_count = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    #print(line)
+
+    word, value = line.split('\t')
+    value = int(value)
+
+    
+    if last_word == word:
+        total_count += value
+    else:
+        if last_word is not None:
+            print(f'{last_word}\t{total_count}')
+        last_word = word
+        total_count = value
+
+if last_word == word:
+    print(f'{last_word}\t{total_count}')
+
